a254d05fda7f11012893eba2bb5af7bebde61558
Sebastian Broekhoven <s.broekhoven@gmail.com>
2020-11-19 21:34:58 +0100

    check

--- libs/ns/main.go
+++ libs/ns/main.go
@@ -12,7 +12,7 @@ func Get(domain string, nameserver string) ([]string, error) {
 	var answer []string
 	m := new(dns.Msg)
 	m.SetQuestion(dns.Fqdn(domain), dns.TypeNS)
-	// m.MsgHdr.RecursionDesired = true
+	m.MsgHdr.RecursionDesired = true
 	// m.SetEdns0(4096, true)
 	c := new(dns.Client)
 	in, _, err := c.Exchange(m, nameserver+":53")
